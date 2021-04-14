baa400712418446868b1c7d78e85b89e517424a1
Gabs Pocevicius <40058873+gabrieliusPocevicius@users.noreply.github.com>
2021-04-14 20:09:57 +0300

    0.6

--- WorldAI/Logic/Character.js
+++ WorldAI/Logic/Character.js
@@ -3,6 +3,7 @@
 // module of names
 "use strict";
 
+
 import {
   names,
   fnames,
@@ -85,6 +86,7 @@ export class Character {
       if (age === 18) {
         adult = true;
         displayNoLongerChild();
+
         clearInterval(aging);
       }
     }, 3000);
@@ -98,7 +100,22 @@ export class Character {
       }
       /* console.log('skills ', skill); */
     }
+          function display(gender, id) {
+            if (age < 18) {
+              displayPerson(gender, id, 14);
 
+              const grow = setInterval(() => {
+                if (play) {
+                  if (age >= 18) {
+                    displayPerson(gender, id);
+                    clearInterval(grow);
+                  }
+                }
+              }, 1000);
+            } else {
+              displayPerson(gender, id);
+            }
+          }
     let percent = 0;
     if (!gender) {
       percent = Math.floor(Math.random() * 99 + 1);
@@ -130,7 +147,7 @@ export class Character {
         console.log(man.name, "has", man.wealth);
       }
 
-      displayPerson(man.gender, man.id);
+      display(man.gender, man.id);
 
       getAttributes = man;
 
@@ -202,7 +219,8 @@ export class Character {
       function wallet() {
         console.log(woman.name, "has", woman.wealth);
       }
-      displayPerson(woman.gender, woman.id);
+
+      display(woman.gender, woman.id);
       getAttributes = woman;
 
       const fertile = setInterval(() => {
@@ -303,7 +321,7 @@ const searchValidMate = (id) => {
     let rightPerson = document.getElementById(`person_${id + 1}`);
     if (rightPerson.classList.contains("female")) {
       console.log("found female on the right");
-      leftPerson.style.opacity = 0.5;
+      rightPerson.style.opacity = 0.5;
     }
   } else {
     let leftPerson = document.getElementById(`person_${id - 1}`);
@@ -332,31 +350,48 @@ function deathClick(e) {
   displayHTML(++deaths, "#deaths", "h5");
 }
 
-export function displayPerson(gender, id) {
-  let color = [52, 58, 64, 1];
-  let person_icon = `
-    <div id="person_${id}" class='d-block ${gender}'>
-      
-       <div class="rounded p-1 m-1" style='background-color:rgba(${color[0]},${color[1]},${color[2]},${color[3]});'>
-            <svg  width="28" height="28" viewBox="0 0 24 24" fill="none">
-                <path  d="M9.53524 21V14.5H9.02349C8.46057 14.5 8 14.05 8 13.5V9C8 7.9 8.92114 7 10.047 7H13.1175C14.2433 7 15.1644 7.9 15.1644 9V13.5C15.1644 14.05 14.7039 14.5 14.1409 14.5H13.6292V21C13.6292 21.55 13.1686 22 12.6057 22H10.5587C9.99581 22 9.53524 21.55 9.53524 21ZM11.5822 6C12.7183 6 13.6292 5.11 13.6292 4C13.6292 2.89 12.7183 2 11.5822 2C10.4461 2 9.53524 2.89 9.53524 4C9.53524 5.11 10.4461 6 11.5822 6Z" fill="#007bff"/>
-            </svg>
-        <div>
-    </div>
-    `;
-  let f_person_icon = `
-    <div id="person_${id}" class='d-block female'>
+export function displayPerson(gender, id, size = 28) {
+ 
+    let color = [52, 58, 64, 1];
+    let person_icon = `
+      <div  class='d-block ${gender}'>
+        
         <div class="rounded p-1 m-1" style='background-color:rgba(${color[0]},${color[1]},${color[2]},${color[3]});'>
-            <svg  width="28" height="28" viewBox="0 0 24 24" fill="none">
-            <path  d="M13.7458 21V16H15.3936C16.0896 16 16.5809 15.33 16.3659 14.68L14.2166 8.37C13.93 7.55 13.1522 7 12.272 7H12.1492C11.269 7 10.4809 7.55 10.2045 8.37L8.0552 14.68C7.83003 15.33 8.32131 16 9.02752 16H10.6753V21C10.6753 21.55 11.1359 22 11.6988 22H12.7223C13.2852 22 13.7458 21.55 13.7458 21ZM12.2106 6C13.3467 6 14.2576 5.11 14.2576 4C14.2576 2.89 13.3467 2 12.2106 2C11.0745 2 10.1636 2.89 10.1636 4C10.1636 5.11 11.0745 6 12.2106 6Z" fill="rgb(255, 99, 132)"/>
-            </svg>
-        </div>
-    </div>
-    `;
-  if (gender == "female") {
-    person_icon = f_person_icon;
+              <svg id="person_${id}" width=${size} height=${size} viewBox="0 0 24 24" fill="none">
+                  <path  d="M9.53524 21V14.5H9.02349C8.46057 14.5 8 14.05 8 13.5V9C8 7.9 8.92114 7 10.047 7H13.1175C14.2433 7 15.1644 7.9 15.1644 9V13.5C15.1644 14.05 14.7039 14.5 14.1409 14.5H13.6292V21C13.6292 21.55 13.1686 22 12.6057 22H10.5587C9.99581 22 9.53524 21.55 9.53524 21ZM11.5822 6C12.7183 6 13.6292 5.11 13.6292 4C13.6292 2.89 12.7183 2 11.5822 2C10.4461 2 9.53524 2.89 9.53524 4C9.53524 5.11 10.4461 6 11.5822 6Z" fill="#007bff"/>
+              </svg>
+          <div>
+      </div>
+      `;
+    let f_person_icon = `
+      <div  class='d-block female'>
+          <div class="rounded p-1 m-1" style='background-color:rgba(${color[0]},${color[1]},${color[2]},${color[3]});'>
+              <svg id="person_${id}" width=${size} height=${size} viewBox="0 0 24 24" fill="none">
+              <path  d="M13.7458 21V16H15.3936C16.0896 16 16.5809 15.33 16.3659 14.68L14.2166 8.37C13.93 7.55 13.1522 7 12.272 7H12.1492C11.269 7 10.4809 7.55 10.2045 8.37L8.0552 14.68C7.83003 15.33 8.32131 16 9.02752 16H10.6753V21C10.6753 21.55 11.1359 22 11.6988 22H12.7223C13.2852 22 13.7458 21.55 13.7458 21ZM12.2106 6C13.3467 6 14.2576 5.11 14.2576 4C14.2576 2.89 13.3467 2 12.2106 2C11.0745 2 10.1636 2.89 10.1636 4C10.1636 5.11 11.0745 6 12.2106 6Z" fill="rgb(255, 99, 132)"/>
+              </svg>
+          </div>
+      </div>
+      `;
+
+    if (gender == "female") {
+      person_icon = f_person_icon;
+    }
+
+  const person = document.getElementById(`person_${id}`);
+
+  if (!person) {
+      console.log('executed display icon');
+      $("people").append(`${person_icon}`); //Creates the icon figure of a person to the screen
+  }else{
+      person.setAttribute('class', 'growth');
+      person.setAttribute("width", `${size}px`);
+      person.setAttribute("height", `${size}px`); 
+      const removed = setTimeout(()=>{ person.classList.remove("growth"); },2000);
+      clearTimeout(removed);
   }
-  $("people").append(`${person_icon}`); //Creates the icon figure of a person to the screen
+    
+  
+
 }
 
 function displayCouple(id) {
@@ -475,7 +510,7 @@ function createPerson(age, gender) {
 }
 
 let adam = createPerson(17, 10);
-let eve = createPerson(30, 60);
+let eve = createPerson(16, 60);
 
 /*
 for(let i = 0;i<10;i++){
