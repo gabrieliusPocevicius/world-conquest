baf92cdc837f9b123d32538cebed55696399b667
gabrielius pocevicius <poceviciusgabrielius@gmail.com>
2022-03-22 14:30:55 +0200

    1.1

--- World/src/App.vue
+++ World/src/App.vue
@@ -1,21 +1,23 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+  import Person from './components/Person.vue'
+
+
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <div v-for="person in 20" key="person">
+    <Person hp='100'/>
+  </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ World/src/components/Person.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  hp: Number
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <p>{{ hp }}</p>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
